eae943ae1ccf99f67930c4dfc04ff59e902b3f01
Lawrence Nigel Anderson <nigel.lawrenceanderson@gmail.com>
2023-09-04 21:50:31 -0500

    all components are function and updates read me

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Basketball-Scout-Site
-git clone
+
+## Initialize the project
+1- Run 'npm install'
+2- 'json-server --watch db.json'
+3- then cd  into phase-1-Basketball-project and run 'explorer.exe index.html'
+
+## Usage
+- Create a new team by filling out the form
+- Create/ add a player to teams
+- Rate each player 
+- Each change persists on the database
--- phase-1-Basketball-project/src/index.js
+++ phase-1-Basketball-project/src/index.js
@@ -14,21 +14,23 @@ const teamList = document.getElementById(`Basketball-Team-list`)
 const teammateList = document.getElementById(`team-mates`)
 const playersInfo = document.getElementById(`players-info`)
 
-// const newTeamFormLoc = document.getElementById(`new-Team`)
-// const addTeamBtn = document.getElementById(`addTeam`)
- const form = document.getElementById('form')
- 
 
+const addTeamBtn = document.getElementById(`addTeam`)
+addTeamBtn.addEventListener("click",() => {
+    const form =document.getElementById(`new-team-form`)
+    form.toggleAttribute("hidden");
+})
 
 
 //need a team logo to pop up in the basketball logos div
  const teamIcon = (team) => { 
     const teamImg = document.createElement(`img`)
+    teamImg.id = "log"
     const delBtn = document.createElement(`button`)
     const logoDiv = document.createElement(`div`)
 
     teamImg.src = team.image
-    delBtn.textContent = "X"
+    // delBtn.textContent = "X"
 
     logoDiv.append(teamImg,delBtn)
     teamList.append(logoDiv)
@@ -38,130 +40,211 @@ const playersInfo = document.getElementById(`players-info`)
         teammateList.textContent = ""
         playersInfo.textContent = ""
         showTeammateList(team) 
+        console.log("sup")
     })
 
     delBtn.addEventListener("click", (e) => {
         e.stopPropagation()
         if(confirm(`are you sure you want to delete this team?`)){
-        fetch(`http://localhost:3000/teams/${team.id}`, {
-        method: "DELETE"
-    })
-    logoDiv.remove()
+            fetch(`http://localhost:3000/teams/${team.id}`, {
+               method: "DELETE"
+             })
+             logoDiv.remove()
+             teammateList.textContent =""
+             playersInfo.textContent =""
 
-}
+        }
     })
 }
 //create card for each player
 const showTeammateList = (team) => {
     teammateList.textContent = ""
-    team.players.forEach((player) => {
+    team = team
+    team.players.forEach((player, index) => {
         const playerHead = document.createElement(`img`)
         const playerName = document.createElement(`h3`)
         const playerDiv = document.createElement(`div`)
-
+        const playerDel = document.createElement('button')
+        
+        playerDiv.id = "hey"
         playerHead.src = player.image
         playerName.textContent = player.name
-        playerDiv.append(playerHead, playerName)
+        
+        playerDiv.append( playerHead, playerName)
         teammateList.append(playerDiv)
     
         playerDiv.addEventListener("click", (e) =>{
             e.stopPropagation()
-            teamMateInfo(player)})
+            teamMateInfo(player)
+        })
+        playerDel.addEventListener("click", (e) => {
+                    e.stopPropagation()
+                    console.log(player)
+        })
+
+
     })
-const newPlayerBtn = document.createElement('button');
-teammateList.append(newPlayerBtn)
-newPlayerBtn.addEventListener('click',() => {
-    const newPlayerform = document.createElement('form')
-    newPlayerform.id = "new-player"
-    const newPlayerName = document.createElement('input')
-    newPlayerName.placeholder = "Name"
-    const newPlayerImage = document.createElement('input')
-    newPlayerImage.placeholder = "Image Url"
-    const newPlayerNumber = document.createElement('input')
-    newPlayerNumber.placeholder = "Number"
-    const newPlayerPoints = document.createElement('input')
-    newPlayerPoints.placeholder = "Points"
-    const newPlayerRebounds = document.createElement('input')
-    newPlayerRebounds.placeholder = "Rebounds"
-    const newPlayerAssists = document.createElement('input')
-    newPlayerAssists.placeholder = "Assists"
-    const newPlayerRatings = document.createElement('input')
-    newPlayerRatings.placeholder = "Ratings"
-   const newPlayerSubmit  = document.createElement('button')
+
+       
+
+     
+      const newPlayerBtn = document.createElement('button');
+      //ADDed this id 
+       teammateList.append(newPlayerBtn) 
+       newPlayerBtn.id = "newPlayerBtn"
+      
+       newPlayerBtn.textContent = "Add Player"
+
+       teammateList.append(newPlayerBtn)
+
+       newPlayerBtn.addEventListener('click',() => {
+            const newPlayerform = document.createElement('form')
+            newPlayerform.id = "new-player"
+            newPlayerform.className = "form-signin"
+
+            const newPlayerName = document.createElement('input')
+            newPlayerName.placeholder = "Name"
+            newPlayerName.className = "form-signin"
+            newPlayerName.className = "form-control"
+
+            const newPlayerImage = document.createElement('input')
+            newPlayerImage.placeholder = "Image Url"
+            newPlayerImage.className ="form-signin"
+            newPlayerImage.className = "form-control"
+
+            const newPlayerNumber = document.createElement('input')
+            newPlayerNumber.className = "form-control"
+            newPlayerNumber.placeholder = "Player Number"
+
+            const newPlayerPoints = document.createElement('input')
+            newPlayerPoints.placeholder = "Points"
+            newPlayerPoints.className = "form-signin"
+            newPlayerPoints.className = "form-control"
+
+            const newPlayerRebounds = document.createElement('input')
+            newPlayerRebounds.placeholder = "Rebounds"
+            newPlayerRebounds.className= "form-signin"
+            newPlayerRebounds.className = "form-control"
+
+            const newPlayerAssists = document.createElement('input')
+            newPlayerAssists.placeholder = "Assists"
+            newPlayerAssists.className = "form-signin"
+            newPlayerAssists.className = "form-control"
+
+            //    const newPlayerRatings = document.createElement('input')
+            //    newPlayerRatings.placeholder = "Ratings"
+            //    newPlayerRatings.className="form-signin"
+            //    newPlayerRatings.className = "form-control"
+
+            const newPlayerSubmit  = document.createElement('button')
+            newPlayerSubmit.className= "form-signin"
+            newPlayerSubmit.className = "w-100 btn btn-lg"
+            newPlayerSubmit.textContent= "Add"
+
+            const formRefresher = document.createElemen
+            
+            newPlayerform.append(newPlayerName, newPlayerImage, newPlayerNumber, newPlayerPoints, newPlayerRebounds, newPlayerAssists, newPlayerSubmit)
+            newPlayerform.id = "newPlayerform"
+            teammateList.append(newPlayerform)
+            newPlayerSubmit.addEventListener(('click'), (e) => {
+                e.preventDefault()
+
+                 const newTeamPlayer ={
+                    name: newPlayerName.value,
+                     image: newPlayerImage.value,
+                    number:  newPlayerNumber.value,
+                     Points : newPlayerPoints.value,
+                    assists : newPlayerAssists.value,
+                    rebounds: newPlayerRebounds.value,
+                    rating : "<img src=\"./hollow.svg\" class=\"ball-style\" width=\"25px\"><img src=\"./hollow.svg\" class=\"ball-style\" width=\"25px\"><img src=\"./hollow.svg\" class=\"ball-style\" width=\"25px\"><img src=\"./hollow.svg\" class=\"ball-style\" width=\"25px\"><img src=\"./hollow.svg\" class=\"ball-style\" width=\"25px\">"
+                }
+
+                let playersList = team.players
+                playersList.push(newTeamPlayer)
+                console.log(playersList)
+
+                let listObj = {
+                    players: playersList
+                }
+                // var parse_obj = JSON.parse(Str_txt);
+                // parse_obj['theTeam'].push({"teamId":"4","status":"pending"});
+                // // Str_txt = JSON.stringify(parse_obj)
+
+                 fetch (`http://localhost:3000/teams/${team.id}`,{
+                    method : 'PATCH',
+                     headers : { 
+                    'Content-Type': "application/json",
+                    "Accept": "application/json",
+                },
+                body: JSON.stringify(listObj)
+
+            })
+            .then(resp => resp.json())
+            .then(data => showTeammateList(team))
+        })
+
+    })
+
+    const teamMateInfo = (player)=> {
+        const playerImg = document.createElement('img')
+        playerImg.id = "howdy"
+        playerImg.src = player.image
+
+        const playerName = document.createElement('h3')
+        playerName.textContent = `Name: ${player.name}`
+
+        const playerNumber = document.createElement('h3')
+        playerNumber.textContent = `Number: ${player.number}`
     
-newPlayerform.append(newPlayerName, newPlayerImage, newPlayerNumber, newPlayerPoints, newPlayerRebounds, newPlayerAssists, newPlayerRatings, newPlayerSubmit)
-teammateList.append(newPlayerform)
-   newPlayerSubmit.addEventListener(('click'), (e) => {
-    e.preventDefault()
-    const newTeamPlayer ={
-        name: newPlayerName.value,
-        image: newPlayerImage.value,
-        number:  newPlayerNumber.value,
-        Points : newPlayerPoints.value,
-        assists : newPlayerAssists.value,
-        rebounds:newPlayerRebounds.value,
-        rating : newPlayerRatings.value
-        }
+        const playerPoints = document.createElement('h3')
+        playerPoints.textContent = `Points: ${player.Points}`
 
-        let playersList = team.players
-        playersList.push(newTeamPlayer)
-        console.log(playersList)
-        let listObj = {
-            players: playersList
-        }
-        // var parse_obj = JSON.parse(Str_txt);
-        // parse_obj['theTeam'].push({"teamId":"4","status":"pending"});
-        // // Str_txt = JSON.stringify(parse_obj)
+        const playerAssists = document.createElement('h3')
+        playerAssists.textContent =`Assists:${player.assists}`
 
-    fetch (`http://localhost:3000/teams/${team.id}`,{
-        method : 'PATCH',
-        headers : { 
-            'Content-Type': "application/json",
-            "Accept": "application/json",
-        },
-          body: JSON.stringify(listObj)
+        const playerRebound = document.createElement('h3')
+        playerRebound.textContent = `Rebound: ${player.rebounds}`
 
-    })
-    .then(resp => resp.json())
-       .then(data => showTeammateList(team))
-   })
+        const playerRating = document.createElement('div')
 
-    })
+        playerRating.innerHTML = player.rating
+
+        const detailDiv = document.createElement(`div`)
+        // const commentBtn = document.createElement(`button`)
+        // const commentInput = document.createElement(`input`)
+
+        const ratingDiv = document.createElement('div')
+        // ratingDiv.id = `balls`
+        
+        playersInfo.textContent = ""
+        detailDiv.append(playerImg, playerName, playerNumber, playerPoints, playerAssists, playerRebound, playerRating)
+        playersInfo.append(detailDiv)
+        ballRater()
+        console.log(player)
+
+        playerRating.addEventListener("click", () =>{
+            player.rating= playerRating.innerHTML
+            let obj = {
+                players: team.players
+            }
+            fetch(`http://localhost:3000/teams/${team.id}`,{
+            method : 'PATCH',
+            headers : { 
+                'Content-Type': "application/json",
+                "Accept": "application/json",
+            },
+            body: JSON.stringify(obj)
+            })
+            console.log(player.rating)
+            console.log(team.players)
+        })
+        
+}
+
+    
 }
 
 
-const teamMateInfo = (player)=> {
-    const playerImg = document.createElement('img')
-    playerImg.src = player.image
-
-    const playerName = document.createElement('h3')
-    playerName.textContent = `Name: ${player.name}`
-
-    const playerNumber = document.createElement('h3')
-    playerNumber.textContent = `Number: ${player.number}`
- 
-    const playerPoints = document.createElement('h3')
-    playerPoints.textContent = `Points: ${player.Points}`
-
-    const playerAssists = document.createElement('h3')
-    playerAssists.textContent =`Assists:${player.assists}`
-
-    const playerRebound = document.createElement('h3')
-    playerRebound.textContent = `Rebound: ${player.rebounds}`
-
-    const playerRating = document.createElement('h3')
-    playerRating.textContent = `Rating: ${player.rating}`
-
-    const detailDiv = document.createElement(`div`)
-    const commentBtn = document.createElement(`button`)
-    const commentInput = document.createElement(`input`)
-    
-    playersInfo.textContent = ""
-    detailDiv.append(playerImg, playerName, playerNumber, playerPoints, playerAssists, playerRebound, playerRating, commentInput, commentBtn)
-    playersInfo.append(detailDiv)
-
-    
-}
 
 
 
@@ -174,7 +257,7 @@ form.addEventListener('submit',function(e){
     const team = {
         image: newImg,
         team:  newName,
-
+        players: []
         }
 
 
@@ -191,5 +274,43 @@ form.addEventListener('submit',function(e){
 
 })
 
+const ballRater = () => {
+    let ballsContainer = document.getElementById("balls")
+
+     let ballImages = document.getElementsByClassName(`ball-style`)
+     Array.from(ballImages).forEach((ballImg, index)=> {   
+
+            ballImg.addEventListener("mouseover", () => onBallHover(index))
+            ballImg.addEventListener("mouseleave", onBallOut)
+            ballImg.addEventListener("click", () => onBallClick(index))
+    })
+        let balls = document.querySelectorAll(".ball-style")
+    
+        let active = -1
+    
+        function onBallHover(i) {
+            fill(i)
+        }
+    
+        function fill (ratingVal) {
+            for(let i = 0; i < 5; i++) {
+                if(i <= ratingVal) {
+                    balls[i].src = "./filledIn.svg"
+                } else {
+                    balls[i].src = "./hollow.svg"
+                }
+            }
+        }
+    
+        function onBallOut(){
+            fill(active)
+        }
+    
+        function onBallClick (i) {
+            active = i
+            // document.getElementById("display-ball-value").innerHTML = i+1
+            fill(active)
+        }
+    }
 
 
